e23889f044258ac77a03b8c84fb2c2e9cd2e83c6
Tomiloba <tikaretomiloba@gmail.com>
2021-01-07 18:07:42 +0100

    Added Admin Add New Product Activity

--- app/src/main/java/com/example/ecommerce/AdminAddNewProductActivity.java
+++ app/src/main/java/com/example/ecommerce/AdminAddNewProductActivity.java
@@ -1,22 +1,32 @@
 package com.example.ecommerce;
 
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AdminAddNewProductActivity extends AppCompatActivity {
 
     private String CategoryName;
+    private Button AddNewProductButton;
+    private ImageView InputProductImage;
+    private EditText InputProductName,InputProductDescription, InputProductPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add_new_product);
 
-
         CategoryName = getIntent().getExtras().get("category").toString();
-Toast.makeText(this, CategoryName
-             , Toast.LENGTH_SHORT).show();
+        AddNewProductButton = (Button)findViewById(R.id.add_new_product);
+        InputProductImage = (ImageView) findViewById(R.id.select_product_image);
+        InputProductName = (EditText) findViewById(R.id.product_name);
+        InputProductPrice = (EditText) findViewById(R.id.product_price);
+
+
+//        Toast.makeText(this, CategoryName
+//             , Toast.LENGTH_SHORT).show();
     }
 }
