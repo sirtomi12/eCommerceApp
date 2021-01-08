03cb96a98b52fb77e78a47d25138a7f3d25ad1fb
Tomiloba <tikaretomiloba@gmail.com>
2021-01-08 23:08:56 +0100

    Added Admin Add New Product Activity

--- app/src/main/java/com/example/ecommerce/AdminAddNewProductActivity.java
+++ app/src/main/java/com/example/ecommerce/AdminAddNewProductActivity.java
@@ -1,18 +1,30 @@
 package com.example.ecommerce;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class AdminAddNewProductActivity extends AppCompatActivity {
 
-    private String CategoryName;
+    private String CategoryName, Description, Price, Pname, saveCurrentDate, saveCurrentTime;
     private Button AddNewProductButton;
     private ImageView InputProductImage;
-    private EditText InputProductName,InputProductDescription, InputProductPrice;
+    private EditText InputProductName, InputProductDescription, InputProductPrice;
+    private static final int GalleryPick = 1;
+    private Uri ImageUri;
+    private String productRandomKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +32,79 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
         setContentView(R.layout.activity_admin_add_new_product);
 
         CategoryName = getIntent().getExtras().get("category").toString();
-        AddNewProductButton = (Button)findViewById(R.id.add_new_product);
+        AddNewProductButton = (Button) findViewById(R.id.add_new_product);
         InputProductImage = (ImageView) findViewById(R.id.select_product_image);
         InputProductName = (EditText) findViewById(R.id.product_name);
         InputProductPrice = (EditText) findViewById(R.id.product_price);
 
-
+        InputProductImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenGallery();
+            }
+        });
+        AddNewProductButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ValidateProductData();
+            }
+        });
 //        Toast.makeText(this, CategoryName
 //             , Toast.LENGTH_SHORT).show();
+    }
+
+
+    private void OpenGallery() {
+
+        Intent galleryIntent = new Intent();
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, GalleryPick);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GalleryPick && resultCode == RESULT_OK && data != null) {
+            ImageUri = data.getData();
+            InputProductImage.setImageURI(ImageUri);
+        }
+    }
+
+    private void ValidateProductData() {
+        Description = InputProductDescription.getText().toString();
+        Price = InputProductPrice.getText().toString();
+        Pname = InputProductName.getText().toString();
+
+        if (ImageUri == null) {
+            Toast.makeText(this, "Product Image is mandatory....", Toast.LENGTH_SHORT)
+                    .show();
+        } else if (TextUtils.isEmpty(Description)) {
+            Toast.makeText(this, "Please write product description...", Toast.LENGTH_SHORT)
+                    .show();
+        } else if (TextUtils.isEmpty(Price)) {
+            Toast.makeText(this, "Please write product price...", Toast.LENGTH_SHORT)
+                    .show();
+        } else if (TextUtils.isEmpty(Pname)) {
+            Toast.makeText(this, "Please write product name...", Toast.LENGTH_SHORT)
+                    .show();
+        }
+        else{
+            StoreProductInformation();
+        }
+    }
+
+    private void StoreProductInformation() {
+        Calendar calendar = Calendar.getInstance();
+
+        SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd, yyyy");
+        saveCurrentDate = currentDate.format(calendar.getTime());
+
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss");
+        saveCurrentTime = currentTime.format(calendar.getTime());
+
+        productRandomKey = saveCurrentDate + saveCurrentTime;
     }
 }
